09e72387846f08484834a2b31a4cbbd6c123c45c
RateMyProf Developer <dev@ratemyprof.in>
2025-10-20 20:24:53 +0530

    fix: Add onCheckedChange prop to Checkbox component

--- frontend/src/components/ui/checkbox.tsx
+++ frontend/src/components/ui/checkbox.tsx
@@ -1,18 +1,26 @@
 import * as React from "react"
 
-const Checkbox = React.forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(({ className, ...props }, ref) => {
-  return (
-    <input
-      type="checkbox"
-      className={`h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-2 focus:ring-blue-500 ${className || ''}`}
-      ref={ref}
-      {...props}
-    />
-  )
-})
+interface CheckboxProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  onCheckedChange?: (checked: boolean) => void
+}
+
+const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ className, onCheckedChange, ...props }, ref) => {
+    return (
+      <input
+        type="checkbox"
+        className={`h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-2 focus:ring-blue-500 ${className || ''}`}
+        ref={ref}
+        onChange={(e) => {
+          if (onCheckedChange) {
+            onCheckedChange(e.target.checked)
+          }
+        }}
+        {...props}
+      />
+    )
+  }
+)
 Checkbox.displayName = "Checkbox"
 
 export { Checkbox }
